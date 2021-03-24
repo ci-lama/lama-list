25e3d41f8b8db098faa06d2008a1a18e46f913b1
Waschndolos <killear@gmx.de>
2021-03-24 17:47:25 +0100

    add test

--- src/app/authentication/services/account.service.spec.ts
+++ src/app/authentication/services/account.service.spec.ts
@@ -3,6 +3,7 @@ import { TestBed } from '@angular/core/testing';
 import { AccountService } from './account.service';
 import {RouterTestingModule} from "@angular/router/testing";
 import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {User} from "../../model/user.model";
 
 describe('AccountService', () => {
   let service: AccountService;
@@ -17,4 +18,14 @@ describe('AccountService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('expect userSubject to be created from localstorage', () => {
+    let user = new User();
+    user.firstName = "hans";
+    localStorage.setItem('user', JSON.stringify(user));
+    service.initUserSubject();
+
+    expect(service.userValue.firstName).toBe("hans")
+  })
+
 });
