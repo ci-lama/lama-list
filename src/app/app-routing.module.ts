5f165170d772a2630d15b0df49a8560d190b0630
Waschndolos <killear@gmx.de>
2021-03-27 08:11:56 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AuthenticationGuard} from "./guards/authentication.guard";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule),
+    canActivate: [AuthenticationGuard]
+  },
+  {
+    path: 'authentication',
+    loadChildren: () => import('./modules/authentication/authentication.module').then(m => m.AuthenticationModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
